d65949a186809f6ceacbd9809dec7a75775ef3d3
Manuel Vasquez <163361779+mavasquez401@users.noreply.github.com>
2024-10-07 21:22:21 -0400

    creating thread

--- chatbot.js
+++ chatbot.js
@@ -30,20 +30,46 @@ const openai = new OpenAI({
 // });
 
 // Create a thread with a user message and an attachment
-// const thread = await openai.beta.threads.create({
-//   messages: [
-//     {
-//       role: 'user',
-//       content: 'I need to solve the equation `3x + 11 = 14`. Can you help me?',
-//       attachments: [
+const thread = await openai.beta.threads.create({
+  messages: [
+    {
+      role: 'user',
+      content: 'I need to solve the equation `3x + 11 = 14`. Can you help me?',
+      attachments: [
+        {
+          file_id: file.id,
+          tools: [{ type: 'code_interpreter' }],
+        },
+      ],
+    },
+  ],
+});
+
+// const main = async () => {
+//   try {
+//     const response = await openai.chat.completions.create({
+//       model: 'gpt-4o',
+//       // 4o works but does not have access to web pages therefore cannot render image content
+//       messages: [
 //         {
-//           file_id: file.id,
-//           tools: [{ type: 'code_interpreter' }],
+//           role: 'user',
+//           content: [
+//             { type: 'text', text: 'write out the menu options' },
+//             {
+//               type: 'image_url',
+//               image_url: {
+//                 url: 'https://static.wixstatic.com/media/914db9_0cfe19a0c9224ef3bdcfe77e4776da5b~mv2.jpg/v1/fill/w_1208,h_1946,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/CN%20Menu%20Jun24.jpg',
+//               },
+//             },
+//           ],
 //         },
 //       ],
-//     },
-//   ],
-// });
+//     });
+//     console.log(response);
+//   } catch (error) {
+//     console.error('Error:', error);
+//   }
+// };
 
 const completion = await openai.chat.completions.create({
   model: 'gpt-4o',
